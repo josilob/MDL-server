2f58bb2245b708dafdc05d95c5bd5b61276c71a1
josilob <bjosilo@gmail.com>
2021-09-06 19:13:02 -0400

    development routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,14 +10,21 @@ const { SECRET = 'defaultSecret' } = process.env;
 // Register route to create a new user
 router.post('/register', async (req, res) => {
 	try {
+		const existingUser = await User.findOne({ username: req.body.username });
+		if (existingUser) {
+			res.json({
+				status: 400,
+				error: `${req.body.username} username already taken.`
+			});
+		}
 		// hash the password
-		req.body.password = await bcrypt.hash(req.body.password);
+		req.body.password = await bcrypt.hash(req.body.password, 10);
 		// create a new user
 		const user = await User.create(req.body);
 		// send new user as response
-		res.json(user);
+		res.json({ status: 200, data: user });
 	} catch (error) {
-		res.status(400).json({ error });
+		res.json({ status: 500, error: error.message });
 	}
 });
 
@@ -44,6 +51,38 @@ router.post('/login', async (req, res) => {
 		}
 	} catch (error) {
 		res.status(400).json({ error });
+	}
+});
+
+// ROUTES FOR DEVELOPMENT
+
+//get all users
+router.get('/get/all', async (req, res) => {
+	try {
+		const allUsers = await User.find();
+		res.json({ message: 'List of all users', allUsers });
+	} catch (error) {
+		res.json({ error: error.message });
+	}
+});
+
+// delete all users
+router.delete('/delete/all', async (req, res) => {
+	try {
+		await User.deleteMany();
+		res.json({ status: 200, message: 'Users collection cleared' });
+	} catch (error) {
+		res.json({ error: error.message });
+	}
+});
+
+// delete specific user
+router.delete('/delete/:username', async (req, res) => {
+	try {
+		await User.findOneAndDelete(req.params.username);
+		res.json({ status: 200, message: `User ${req.params.username} is removed` });
+	} catch (error) {
+		res.json({ error: error.message });
 	}
 });
 
--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
 	{
 		username: { type: String, required: true },
-		password: { type: String, required: true, minlength: 6, trim: true }
-		// email: { type: String, required: true, unique: true, trim: true },
+		password: { type: String, required: true, minlength: 6, trim: true },
+		email: { type: String, required: true, unique: true, trim: true }
 		// favoriteDrinks: [
 		// 	{ type: mongoose.Schema.Types.ObjectId, ref: 'FavoriteDrink' }
 		// ]
